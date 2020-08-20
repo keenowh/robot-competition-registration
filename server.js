28f0b7a119b9f5c464d81776182c94ff0420723f
Keen Ballon <keenballon@gmail.com>
2020-08-20 23:20:18 +0800

    try

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   // to serve static content
   app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, (err) => {
