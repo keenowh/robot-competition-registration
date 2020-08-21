673b96e4c2fa80315fbd14498c8688051a807a9e
Keen Ballon <keenballon@gmail.com>
2020-08-21 14:08:20 +0800

    removed redis

--- api/register.js
+++ api/register.js
@@ -1,27 +1,27 @@
 const express = require("express");
 
-const Redis = require("ioredis");
 const router = express.Router();
 const pool = require("../db");
 
-const redis = new Redis(6379);
+// const Redis = require("ioredis");
+// const redis = new Redis(6379);
 
-function cache(req, res, next) {
-  redis.get("competitors", (err, result) => {
-    if (err) throw err;
-    if (result !== null) {
-      console.log("I'm using cache");
-      res.status(200).send(JSON.parse(result));
-    } else {
-      next();
-    }
-  });
-}
+// function cache(req, res, next) {
+//   redis.get("competitors", (err, result) => {
+//     if (err) throw err;
+//     if (result !== null) {
+//       console.log("I'm using cache");
+//       res.status(200).send(JSON.parse(result));
+//     } else {
+//       next();
+//     }
+//   });
+// }
 
-router.get("/", cache, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const response = await pool.query("SELECT * FROM competitors");
-    redis.set("competitionrs", JSON.stringify(response.rows));
+    // redis.set("competitionrs", JSON.stringify(response.rows));
 
     res.status(200).json(response.rows);
   } catch (err) {
@@ -41,7 +41,7 @@ router.post("/", async (req, res) => {
       "INSERT INTO competitors (compName, players, school, coach) VALUES ($1, $2, $3, $4) RETURNING *",
       [info.compName, info.players, info.school, info.coach]
     );
-    redis.del("competitors");
+    // redis.del("competitors");
     console.log(response.rows);
     res.status(200).json(response.rows);
   } catch (err) {
