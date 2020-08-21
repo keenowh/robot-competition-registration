161592ea8b86f8fe3a204ff6598c06a2b529e4b3
Keen Ballon <keenballon@gmail.com>
2020-08-21 13:16:31 +0800

    Changed db.js

---
+++ client/build/precache-manifest.1183fe489d8051774096739a7f8cc22c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc8068e313f99dc0e21bca83cf66d6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c21f3067e991340068a",
+    "url": "/static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "361df068436d47647e0c",
+    "url": "/static/css/main.3473e67f.chunk.css"
+  },
+  {
+    "revision": "4c21f3067e991340068a",
+    "url": "/static/js/2.cdc3d175.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/static/js/2.cdc3d175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "361df068436d47647e0c",
+    "url": "/static/js/main.2d02fe12.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- db.js
+++ db.js
@@ -1,23 +1,27 @@
-const Pool = require("pg").Pool;
+const pg = require("pg");
 
-const devPool = {
-  user: "postgres",
-  password: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "competition",
-};
+const devPool = () =>
+  new pg.Pool({
+    user: "postgres",
+    password: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "competition",
+  });
 
-const prodPool = {
-  user: process.env.user,
-  password: process.env.password,
-  host: process.env.host,
-  port: 5432,
-  database: process.env.database,
-};
+const prodPool = () =>
+  new pg.Pool({
+    user: "tbfkwytxxysxgd",
+    password:
+      "76b4ae0357c597b017eebce3ad01acd56a3e41b278cce0dc6f56ca0c46bee93f",
+    host: "ec2-184-72-162-198.compute-1.amazonaws.com",
+    port: 5432,
+    database: "d9hph6uvbmpt6l",
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? prodPool : devPool
-);
+const pool = process.env.NODE_ENV === "production" ? prodPool() : devPool();
 
 module.exports = pool;
